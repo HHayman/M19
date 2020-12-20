3f5d48d818741493d3a5397c7a0d27338e6bb95a
HHayman <l.hayman.1@research.gla.ac.uk>
2020-12-20 18:04:37 +0000

    Update rCut test.

--- R/rCut.R
+++ R/rCut.R
@@ -15,40 +15,51 @@
 
 
 #Function
-rCut <- function(Data, CSS = "No", OS = "No", MFS = "No", ID = "ID", Survival = "SurvivalTime", SurvivalStatus = "SurvivalStatus", Progression = "ProgressionTime", ProgressionStatus = "ProgresionStatus", Variable1 = "Variable1", Variable1Unit = "Variable1Unit", Variable2 = "Variable2", Variable2Unit = "Variable2Unit", Variable3 = "Variable3", Variable3Unit = "Variable3Unit", Variable4 = "Variable4", Variable4Unit = "Variable4Unit", Variable5 = "Variable5", Variable5Unit = "Variable5Unit", Variable6 = "Variable6", Variable6Unit = "Variable6Unit", Variable7 = "Variable7", Variable7Unit = "Variable7Unit", Variable8 = "Variable8", Variable8Unit = "Variable8Unit") {
+rCut <- function(Data, CSS = "No", OS = "No", PFS = "No", ID = "ID", Survival = "SurvivalTime", SurvivalStatus = "SurvivalStatus", Progression = "ProgressionTime", ProgressionStatus = "ProgresionStatus", Variables)
+{
+
+
+  if (!inherits(data, "data.frame"))
+    stop("data should be an object of class data.frame")
+  data <- as.data.frame(data)
+  if (!all(c(ID, Survival, SurvivalStatus, Progression, ProgressionStatus) %in% colnames(data)))
+    stop("Specify correct column names containing ID, Survival, SurvivalStatus, Progression and ProgressionStatusvalues.")
+  if (!all(Variables %in% colnames(data)))
+    stop("Some variables are not found in the data: ",
+         paste(setdiff(variables, colnames(data)), collapse = ", "))
 
 
 
-  names(Data)[names(Data) == ID] <- "ID"
-  names(Data)[names(Data) == Survival] <- "SurvivalTime"
-  names(Data)[names(Data) == SurvivalStatus] <- "SurvivalStatus"
-  names(Data)[names(Data) == Progression] <- "ProgressionTime"
-  names(Data)[names(Data) == ProgressionStatus] <- "ProgressionStatus"
-  names(Data)[names(Data) == Variable1] <- "Variable1"
-  names(Data)[names(Data) == Variable1Unit] <- "Variable1Unit"
-  names(Data)[names(Data) == Variable2] <- "Variable2"
-  names(Data)[names(Data) == Variable2Unit] <- "Variable2Unit"
-  names(Data)[names(Data) == Variable3] <- "Variable3"
-  names(Data)[names(Data) == Variable3Unit] <- "Variable3Unit"
-  names(Data)[names(Data) == Variable4] <- "Variable4"
-  names(Data)[names(Data) == Variable4Unit] <- "Variable4Unit"
-  names(Data)[names(Data) == Variable5] <- "Variable5"
-  names(Data)[names(Data) == Variable5Unit] <- "Variable5Unit"
-  names(Data)[names(Data) == Variable6] <- "Variable6"
-  names(Data)[names(Data) == Variable6Unit] <- "Variable6Unit"
-  names(Data)[names(Data) == Variable7] <- "Variable7"
-  names(Data)[names(Data) == Variable7Unit] <- "Variable7Unit"
-  names(Data)[names(Data) == Variable8] <- "Variable8"
-  names(Data)[names(Data) == Variable8Unit] <- "Variable8Unit"
-
-
-
+  #Splits survival status into CSS and OS status
   Data$CancerSpecificStatus <- ifelse(Data$SurvivalStatus == 1, c("1"), c("0"))
   Data$OverallSpecificStatus <- ifelse(Data$SurvivalStatus > 0, c("1"), c("0"))
 
-  NewData <- Data
 
-  NewData
 
-  return(NewData)
+  #Determine cut off for cancer-specific survival
+  if (CSS == "Yes") {
+    res.cut <- surv_cutpoint(Data, time = Survival, event = CSS, Variables)
+    plot(res.cut, Variables, palette = c("#d70033", "#5596e6"))
+  }
+
+
+
+  #Determine cut off for overall survival
+  if (OS == "Yes") {
+    res.cut <- surv_cutpoint(Data, time = Survival, event = OS, Variables)
+    plot(res.cut, Variables, palette = c("#d70033", "#5596e6"))
+  }
+
+
+
+  #Determine cut off for progression-free survival
+  if (PFS == "Yes") {
+    res.cut <- surv_cutpoint(Data, time = Progression, event = ProgressionStatus, Variables)
+    plot(res.cut, Variables, palette = c("#d70033", "#5596e6"))
+  }
+
+
+
+
+
 }
