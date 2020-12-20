5a4d9f136441440de147fb0bc471f3a6c76a7c87
HHayman <l.hayman.1@research.gla.ac.uk>
2020-12-20 13:52:41 +0000

    Introduce initial rCut for testing, and update imports for ImmSurv.

--- R/ImmSurv.R
+++ R/ImmSurv.R
@@ -3,15 +3,9 @@
 
 #' This function plots two variables on a scatter plot and contextualises the plot using survival time.
 
-#' @import devtools
-#' @import maftools
 #' @import ggpubr
 #' @import ggplot2
-#' @import survminer
-#' @import tidyverse
-#' @import roxygen2
-#' @import survival
-#' @import maxstat
+
 
 #' @export
 
