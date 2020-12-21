ba78449709e06ecb132cc63e2ad727bfcda9c023
HHayman <l.hayman.1@research.gla.ac.uk>
2020-12-21 16:07:06 +0000

    Update rCut test.

--- R/rCut.R
+++ R/rCut.R
@@ -71,12 +71,19 @@ rCut <- function(Data, CSS, OS, PFS, ID, Survival, SurvivalStatus, Progression, 
   return(Plots)
 
   pdf("C:/Users/liamj/Desktop/R/Plots.pdf")
-  invisible(lapply(Plots, print))
+  if (CSS == "Yes") {
+    CSS_Plots <- plot(CSS.res.cut, Variables, palette = c("#d70033", "#5596e6"), main=list(font=2, cex=5, label="Cancer-Specific Survival"))
+    print(CSS_Plots)
+  }
+  if (OS == "Yes") {
+    OS_Plots <- plot(OS.res.cut, Variables, palette = c("#d70033", "#5596e6"), main="Overall Survival", font.main=2, cex.main=5)
+    print(OS_Plots)
+  }
+  if (PFS == "Yes") {
+    PFS_Plots <- plot(PFS.res.cut, Variables, palette = c("#d70033", "#5596e6"), main="Progression-Free Survival", font.main=2, cex.main=5)
+    print(PFS_Plots)
+  }
   dev.off()
 
-
-  #pdf("gridplots.pdf", onefile = TRUE)
-  #do.call("grid.arrange", Plots)
-  #dev.off()
 
 }
