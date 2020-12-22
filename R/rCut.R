3a8e20653f79fef5708af0ae956253ad607457fe
HHayman <l.hayman.1@research.gla.ac.uk>
2020-12-22 12:01:58 +0000

    Update rCut test.

--- R/rCut.R
+++ R/rCut.R
@@ -71,17 +71,18 @@ rCut <- function(Data, CSS, OS, PFS, ID, Survival, SurvivalStatus, Progression, 
   return(Plots)
 
   pdf("C:/Users/liamj/Desktop/R/Plots.pdf")
-  print(Plots)
-  dev.off()
-
-  pdf("C:/Users/liamj/Desktop/R/Plots2.pdf")
   CSS_Plots <- plot(CSS.res.cut, Variables, palette = c("#d70033", "#5596e6"), main=list(font=2, cex=5, label="Cancer-Specific Survival"))
-  OS_Plots <- plot(OS.res.cut, Variables, palette = c("#d70033", "#5596e6"), main="Overall Survival", font.main=2, cex.main=5)
-  PFS_Plots <- plot(PFS.res.cut, Variables, palette = c("#d70033", "#5596e6"), main="Progression-Free Survival", font.main=2, cex.main=5)
   print(CSS_Plots)
-  print(OS_Plots)
-  print(PFS_Plots)
-  dev.off()
+  graphics.off()
+
+  #pdf("C:/Users/liamj/Desktop/R/Plots2.pdf")
+  #CSS_Plots <- plot(CSS.res.cut, Variables, palette = c("#d70033", "#5596e6"), main=list(font=2, cex=5, label="Cancer-Specific Survival"))
+  #OS_Plots <- plot(OS.res.cut, Variables, palette = c("#d70033", "#5596e6"), main="Overall Survival", font.main=2, cex.main=5)
+  #PFS_Plots <- plot(PFS.res.cut, Variables, palette = c("#d70033", "#5596e6"), main="Progression-Free Survival", font.main=2, cex.main=5)
+  #print(CSS_Plots)
+  #print(OS_Plots)
+  #print(PFS_Plots)
+  #dev.off()
 
 
 }
