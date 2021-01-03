71641b1594ebeb131c60d668cb6f5d12d2183626
HHayman <l.hayman.1@research.gla.ac.uk>
2021-01-03 16:29:33 +0000

    Update rCut test.

--- R/rCut.R
+++ R/rCut.R
@@ -181,14 +181,14 @@ rCut <- function(Data, CSS, OS, PFS, PlotPalette="SPSS", ID, Survival, SurvivalS
 
 
 
-              n = length(Variables)
-              for (i in 1:n) {
-                png(paste0("CSS_", Variables[i], ".png"))
-                CSS.res.cut <- surv_cutpoint(Data, time = "Survival", event = "CSS", Variables[i])
-                SinglePlot <- plot(CSS.res.cut, Variables[i], palette = c("#d70033", "#5596e6"), main="Cancer-Specific Survival")
-                print(SinglePlot)
-                dev.off()
-              }
+              #n = length(Variables)
+              # for (i in 1:n) {
+              #png(paste0("CSS_", Variables[i], ".png"))
+              #CSS.res.cut <- surv_cutpoint(Data, time = "Survival", event = "CSS", Variables[i])
+              # SinglePlot <- plot(CSS.res.cut, Variables[i], palette = c("#d70033", "#5596e6"), main="Cancer-Specific Survival")
+              #print(SinglePlot)
+              #dev.off()
+              #}
 
 
 
